aeebc635ee5b26b6ec00c37008172eedd185ef21
lettemax <maxlettenberger@gmail.com>
2015-05-20 14:23:01 -0700

    Initial Commit

---
+++ HallOfFrame/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HallOfFrame
+//
+//  Created by Max Lettenberger on 5/20/15.
+//  Copyright (c) 2015 Max. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HallOfFrame/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HallOfFrame
+//
+//  Created by Max Lettenberger on 5/20/15.
+//  Copyright (c) 2015 Max. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
